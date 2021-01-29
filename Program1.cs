807a70b0f948d6d7531767d8eb791ed1dacce2de
numanaktas <75180928+numanaktas@users.noreply.github.com>
2021-01-29 15:43:39 +0300

    Proje dosyası ekle.

---
+++ Program1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Metotlar;
+namespace Metotlar
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Product urun1 = new Product();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya elması";
+
+            Product urun2 = new Product();
+            urun2.Adi = "karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "diyarbakır karpuzu";
+
+            Product[] urunler = new Product[] { urun1, urun2 };
+
+            foreach (Product urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("------------------------");
+            }
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("elma","yeşil elma",15);
+            sepetManager.Ekle2("armut", "yeşil armut", 18);
+
+
+        }
+
+    }
+}
